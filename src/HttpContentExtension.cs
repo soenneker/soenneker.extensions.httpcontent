ff49076f55498abfc0fe5e97d8b9863cc1c62eac
Jake Soenneker <jake@soenneker.com>
2024-06-28 11:46:11 -0500

    AddCookie added

--- src/HttpContentExtension.cs
+++ src/HttpContentExtension.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections.Generic;
 using System.Net.Http;
+using System.Text;
 using System.Threading.Tasks;
 using Soenneker.Extensions.Task;
 
@@ -12,7 +14,7 @@ public static class HttpContentExtension
 {
     public static async ValueTask<System.Net.Http.HttpContent> Clone(this System.Net.Http.HttpContent content)
     {
-        if (content == null) 
+        if (content == null)
             return null;
 
         var ms = new System.IO.MemoryStream();
@@ -28,5 +30,51 @@ public static class HttpContentExtension
         }
 
         return result;
+    }
+
+    /// <summary>
+    /// Adds a cookie to the HTTP content's headers.
+    /// </summary>
+    /// <param name="content">The HTTP content to which the cookie will be added.</param>
+    /// <param name="cookieName">The name of the cookie.</param>
+    /// <param name="cookieValue">The value of the cookie.</param>
+    /// <param name="domainOrUri">
+    /// The domain or URI to be used for the cookie.
+    /// If both <paramref name="domainOrUri"/> and <paramref name="path"/> are provided, 
+    /// <paramref name="domainOrUri"/> will be treated as the domain.
+    /// If only <paramref name="domainOrUri"/> is provided, it will be treated as a URI.
+    /// </param>
+    /// <param name="path">
+    /// The path to be used for the cookie.
+    /// If <paramref name="path"/> is not provided and <paramref name="domainOrUri"/> is a URI, 
+    /// the path will be extracted from the URI.
+    /// If <paramref name="path"/> is not provided and <paramref name="domainOrUri"/> is a domain, 
+    /// the default path will be set to "/".
+    /// </param>
+    public static void AddCookie(this System.Net.Http.HttpContent content, string cookieName, string cookieValue, string domainOrUri, string? path = null)
+    {
+        string domain;
+
+        if (path == null && Uri.TryCreate(domainOrUri, UriKind.Absolute, out Uri? uri))
+        {
+            domain = uri.Host;
+            path ??= uri.AbsolutePath;
+        }
+        else
+        {
+            domain = domainOrUri;
+            path ??= "/";
+        }
+
+        StringBuilder cookieBuilder = new StringBuilder(cookieName.Length + cookieValue.Length + domain.Length + path.Length + 20)
+            .Append(cookieName)
+            .Append('=')
+            .Append(cookieValue)
+            .Append("; Domain=")
+            .Append(domain)
+            .Append("; Path=")
+            .Append(path);
+
+        content.Headers.Add("Cookie", cookieBuilder.ToString());
     }
 }
